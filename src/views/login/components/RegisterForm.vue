bb20cdfb20e02580a50024bdb9b9d96c507f7ac2
谢丹燕 <zt000922@zt.com>
2026-02-10 16:49:00 +0800

    fix: 邮箱校验规则不一致

--- src/views/login/components/RegisterForm.vue
+++ src/views/login/components/RegisterForm.vue
@@ -193,6 +193,15 @@ const validatePassword = (_rule: any, value: string, callback: any) => {
   }
 };
 
+/* eslint-disable no-control-regex */
+const emailPattern =
+  /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i;
+/* eslint-enable no-control-regex */
+
+const isEmailValid = computed(() => {
+  return emailPattern.test(registerData.email);
+});
+
 const registerRules = reactive<FormRules>({
   email: [
     {
@@ -209,10 +218,6 @@ const registerRules = reactive<FormRules>({
     }
   ],
   password: [{ required: true, validator: validatePassword, trigger: "change" }]
-});
-
-const isEmailValid = computed(() => {
-  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(registerData.email);
 });
 
 const hasLowercase = computed(() => /[a-z]/.test(registerData.password));
