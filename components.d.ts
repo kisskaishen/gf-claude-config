ec6cea6582a6802bc5e0908698fd39fcf1189b96
秦文凯 <qinwenkai@gofo.com>
2026-04-17 18:54:50 +0800

    feat: 订单详情

--- components.d.ts
+++ components.d.ts
@@ -22,6 +22,7 @@ declare module 'vue' {
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
     ElDialogFooter: typeof import('element-plus/es')['ElDialogFooter']
+    ElDivider: typeof import('element-plus/es')['ElDivider']
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
