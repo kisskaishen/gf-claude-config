1c24729653ab72e76de1a174fe5bb481e172d963
秦文凯 <qinwenkai@gofo.com>
2026-04-17 15:11:28 +0800

    feat: 添加校验限制

--- components.d.ts
+++ components.d.ts
@@ -21,6 +21,7 @@ declare module 'vue' {
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
+    ElDialogFooter: typeof import('element-plus/es')['ElDialogFooter']
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
