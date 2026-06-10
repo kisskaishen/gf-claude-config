41fc71edfc32bd8a42b2ba9b95adb43f5700e5bf
秦文凯 <qinwenkai@gofo.com>
2026-06-10 14:01:27 +0800

    feat: 优化成功对话框按钮样式，支持文字换行并调整宽度

--- src/views/home/gigma.vue
+++ src/views/home/gigma.vue
@@ -263,7 +263,7 @@
 
     <!-- 右侧帮助和反馈 -->
     <div
-      class="fixed right-0 flex flex-col gap-2 -translate-y-1/2 bg-white shadow-md bottom-1/4 px-1.5 py-3"
+      class="hidden fixed right-0 flex-col gap-2 -translate-y-1/2 bg-white shadow-md bottom-1/4 px-1.5 py-3"
     >
       <!-- 帮助 -->
       <div
